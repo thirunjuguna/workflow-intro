c438e047e12b0a949bd774385b8d3da99ef5b2a9
Thiru Njuguna <uriht@outlook.com>
2017-03-05 21:04:32 +0300

    ISSUE 002 strict check

---
+++ issue-002/solution.js
@@ -0,0 +1,16 @@
+/* 
+ * THIRU NJUGUNA
+ * uriht@outlook.com
+ * Made with love
+ */
+function strictCheck(a, b) {
+    if(a===b){
+        return 1;
+    }/*else if(a==b){
+        return true;
+    }else{
+        return false;
+    }*/
+}
+
+console.log(strictCheck(1,1));
---
+++ issue-002/tests.js
@@ -0,0 +1,22 @@
+/**
+ * Do not change anything on this file
+ * And don't worry about it for now
+ * For more info, check https://jasmine.github.io/
+ */
+describe("Strict Check:", function() {
+    it("checks for same type", function() {
+        expect(strictCheck(false, false)).toEqual(1);
+        expect(strictCheck(30, 30)).toEqual(1);
+    });
+
+    it("checks for loosely equal", function() {
+        expect(strictCheck(10, "10")).toEqual(true);
+        expect(strictCheck("", 0)).toEqual(true);
+        expect(strictCheck([], 0)).toEqual(true);
+    });
+
+    it("checks for not equal", function() {
+        expect(strictCheck(10, "10.5")).toEqual(false);
+        expect(strictCheck({}, 0)).toEqual(false);
+    });
+});
